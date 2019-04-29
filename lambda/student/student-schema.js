59b0ade9683424592404ef0aef8890310a6394e8
mschmidt712 <mschmidt@kenzan.com>
2019-04-29 13:24:17 -0600

    Update student schema.

--- lambda/student/student-schema.js
+++ lambda/student/student-schema.js
@@ -3,6 +3,8 @@ const joi = require('joi');
 exports.schema = joi.object().keys({
     name: joi.string().min(3).max(30).required(),
     email: joi.string().email({ minDomainAtoms: 2 }).required(),
+    id: joi.string().required(),
+    loginMethod: joi.string().valid('cognito', 'facebook', 'google').required(),
     phoneNumber: joi.string().min(10),
     hippaCert: joi.boolean(),
     address: joi.object().keys({
@@ -12,4 +14,4 @@ exports.schema = joi.object().keys({
         zipCode: joi.number().integer().required()
     }),
     requests: joi.array()
-  });
+});
